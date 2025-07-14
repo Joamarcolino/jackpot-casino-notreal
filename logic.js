7ab25607e62705b079f12d45e6aceb33d242fe86
João Vitor Marcolino Veiga <166542665+Joamarcolino@users.noreply.github.com>
2025-07-14 17:00:42 -0300

    Update 2
    - Added Money count.
    - Added Roll count.
    - Added instructions h1.
    - DId some adjustments to UI.
    - Added an statistics part.
    - Added a custom win popup.
    - reworked the gambling image position

--- logic.js
+++ logic.js
@@ -1,33 +1,53 @@
-const slotone = document.querySelector("#slot1")
-const slottwo = document.querySelector("#slot2")
-const slotthree = document.querySelector("#slot3")
-const rollbutton = document.querySelector("#slotroll")
-const gamblingwinnning = document.querySelector("#wingambling")
-let wins = 0
-let rolls = 0
+const slotone = document.querySelector("#slot1");
+const slottwo = document.querySelector("#slot2");
+const slotthree = document.querySelector("#slot3");
+const rollbutton = document.querySelector("#slotroll");
+const gamblingwinnning = document.querySelector("#wingambling");
+const wincount = document.querySelector("#win-count");
+const rollcount = document.querySelector("#roll-count");
+const moneycount = document.querySelector("#money-count");
+
+let wins = 0;
+let rolls = 0;
+let money = 0;
 
 function rollvalues() {
-    const num1 = Math.floor(Math.random() * 10) + 1
-    const num2 = Math.floor(Math.random() * 10) + 1
-    const num3 = Math.floor(Math.random() * 10) + 1
+    const num1 = Math.floor(Math.random() * 10) + 1;
+    const num2 = Math.floor(Math.random() * 10) + 1;
+    const num3 = Math.floor(Math.random() * 10) + 1;
 
-    rolls++
+    rolls = rolls + 0.5;
+    rollcount.innerHTML = rolls;
 
-    slotone.innerHTML = num1
-    slottwo.innerHTML = num2
-    slotthree.innerHTML = num3
+    slotone.innerHTML = num1;
+    slottwo.innerHTML = num2;
+    slotthree.innerHTML = num3;
 
-    victories(num1, num2, num3)
+    victories(num1, num2, num3);
+}
+function showPopup(message) {
+    const popup = document.getElementById("popup");
+    popup.innerText = message;
+    popup.style.display = "block";
+
+    setTimeout(() => {
+        popup.style.display = "none";
+    }, 4000);
 }
 
 function victories(num1, num2, num3) {
     if (num1 === num2 && num2 === num3) {
-        wins++
-        alert(`Goodjob! You now have ${wins} wins!`)
-        wingambling.style.display = "flex";
+        wins++;
+        money += 10000;
+        gamblingwinnning.style.display = "flex";
+        showPopup("You won $10.000! Goodjob buddy!")
+        setTimeout(() => {
+            gamblingwinnning.style.display = "none";
+        }, 4000);
     }
-    setTimeout(() => {
-    wingambling.style.display = "none";
-}, 9000);
+
+    wincount.innerHTML = wins;
+    moneycount.innerHTML = `$${money.toLocaleString()}`;
 }
-rollbutton.addEventListener("click", rollbutton)
+
+rollbutton.addEventListener("click", rollvalues);
